c9fcbb5bd76c75f80f18aa9f1c277072b9c29e01
nanda <crea.nanda@gmail.com>
2020-05-20 15:26:52 +0200

    format index.js & add batch replace

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,4 @@
-var Generator = require('yeoman-generator');
+var Generator = require("yeoman-generator");
 
 class MyBase extends Generator {
   capitalizeFirstLetter(string) {
@@ -13,101 +13,220 @@ module.exports = class extends MyBase {
     super(args, opts);
 
     // This makes `appname` a required argument.
-    this.argument("entityName", { type: String, required: true});
+    this.argument("entityName", { type: String, required: true });
 
     this.entityName = this.options.entityName;
-    this.entityNameCapitalized = this.capitalizeFirstLetter(this.options.entityName);
+    this.entityNameCapitalized = this.capitalizeFirstLetter(
+      this.options.entityName
+    );
   }
 
   writing() {
-      this.fs.copyTpl(
-      this.templatePath('AddEntity.vue'),
-      this.destinationPath('public/src/components/Add' + this.entityNameCapitalized + '.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("AddEntity.vue"),
+      this.destinationPath(
+        "public/src/components/Add" + this.entityNameCapitalized + ".vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('EntityDetail.vue'),
-      this.destinationPath('public/src/components/' + this.entityNameCapitalized + 'Detail.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("EntityDetail.vue"),
+      this.destinationPath(
+        "public/src/components/" + this.entityNameCapitalized + "Detail.vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('EntityItem.vue'),
-      this.destinationPath('public/src/components/' + this.entityNameCapitalized + 'Item.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("EntityItem.vue"),
+      this.destinationPath(
+        "public/src/components/" + this.entityNameCapitalized + "Item.vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('EntityList.vue'),
-      this.destinationPath('public/src/components/' + this.entityNameCapitalized + 'List.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("EntityList.vue"),
+      this.destinationPath(
+        "public/src/components/" + this.entityNameCapitalized + "List.vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('user-entities-db.js'),
-      this.destinationPath('public/src/firebase/user-' + this.entityName + 's' + '-db.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("user-entities-db.js"),
+      this.destinationPath(
+        "public/src/firebase/user-" + this.entityName + "s" + "-db.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('index.js'),
-      this.destinationPath('public/src/store/' + this.entityName + 's' + '/' + 'index.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("index.js"),
+      this.destinationPath(
+        "public/src/store/" + this.entityName + "s" + "/" + "index.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.actions.js'),
-      this.destinationPath('public/src/store/' + this.entityName + 's' + '/' + this.entityName + 's' + '.actions.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.actions.js"),
+      this.destinationPath(
+        "public/src/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".actions.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.getters.js'),
-      this.destinationPath('public/src/store/' + this.entityName + 's' + '/' + this.entityName + 's' + '.getters.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.getters.js"),
+      this.destinationPath(
+        "public/src/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".getters.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.mutations.js'),
-      this.destinationPath('public/src/store/' + this.entityName + 's' + '/' + this.entityName + 's' + '.mutations.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.mutations.js"),
+      this.destinationPath(
+        "public/src/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".mutations.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.state.js'),
-      this.destinationPath('public/src/store/' + this.entityName + 's' + '/' + this.entityName + 's' + '.state.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.state.js"),
+      this.destinationPath(
+        "public/src/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".state.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('Entity.vue'),
-      this.destinationPath('public/src/views/' + this.entityNameCapitalized + '.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("Entity.vue"),
+      this.destinationPath(
+        "public/src/views/" + this.entityNameCapitalized + ".vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('Entities.vue'),
-      this.destinationPath('public/src/views/' + this.entityNameCapitalized + 's' + '.vue'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("Entities.vue"),
+      this.destinationPath(
+        "public/src/views/" + this.entityNameCapitalized + "s" + ".vue"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.actions.spec.js'),
-      this.destinationPath('public/tests/unit/store/' + this.entityName + 's' + "/" + this.entityName + 's' + '.actions.spec.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.actions.spec.js"),
+      this.destinationPath(
+        "public/tests/unit/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".actions.spec.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.getters.spec.js'),
-      this.destinationPath('public/tests/unit/store/' + this.entityName + 's' + "/" + this.entityName + 's' + '.getters.spec.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.getters.spec.js"),
+      this.destinationPath(
+        "public/tests/unit/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".getters.spec.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
 
-      this.fs.copyTpl(
-      this.templatePath('entities.mutations.spec.js'),
-      this.destinationPath('public/tests/unit/store/' + this.entityName + 's' + "/" + this.entityName + 's' + '.mutations.spec.js'),
-      { entityNameCapitalized: this.entityNameCapitalized, entityName: this.entityName }
+    this.fs.copyTpl(
+      this.templatePath("entities.mutations.spec.js"),
+      this.destinationPath(
+        "public/tests/unit/store/" +
+          this.entityName +
+          "s" +
+          "/" +
+          this.entityName +
+          "s" +
+          ".mutations.spec.js"
+      ),
+      {
+        entityNameCapitalized: this.entityNameCapitalized,
+        entityName: this.entityName,
+      }
     );
   }
 };
